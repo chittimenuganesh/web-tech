f4649bb19082c35f69087f9ae6af85c627bb2d6a
chittimenuganesh <chganesh2005@gmail.com>
2026-02-11 06:02:46 +0530

    res-query

--- backend/node-js-practice/server.js
+++ backend/node-js-practice/server.js
@@ -13,3 +13,22 @@ app.get("/", (req,res)=> {
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
+
+app.get('/great', (req,res) => {
+  const name = req.query.name;
+  res.send(`hello ${name}`);
+});
+
+app.get('/about', (req,res)=>{
+  res.send('this is about page');
+});
+
+app.get('/sum', (req, res) => {
+  let a = Number(req.query.a);
+  let b = Number(req.query.b);
+
+
+  console.log(req.query);
+
+  res.send(`Sum is ${a + b}`);
+});
